c884e8d7e837d42c40b2931c97ea203f3d68973f
Liubaba <apple@appledeMacBook-Pro.local>
2020-04-09 00:06:38 +0800

    播放页面一版(未完成 1)

--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,11 +1,28 @@
-import {formatTimeSecond}  from '../../utils/common'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    songinfo: {
+      album: "麻雀",
+      duration: 252,
+      filename: "C400000lv3Zi13dSVA.m4a",
+      id: 246492240,
+      image: "https://y.gtimg.cn/music/photo_new/T002R300x300M000003eE8gA3TfuKc.jpg?max_age=2592000",
+      isonly: 0,
+      mid: "000lv3Zi13dSVA",
+      msgid: 14,
+      name: "麻雀",
+      rate: 23,
+      singer: "李荣浩"
+    },
+    song: {
+      duration: 0,
+      name: '',
+      currentduration: 0,
+      progres: 0
+    }
   },
 
   /**
@@ -17,9 +34,30 @@ Page({
     console.log(song)
     const innerAudioContext = wx.createInnerAudioContext()
     innerAudioContext.autoplay = true
-    innerAudioContext.src = song.url
+    var _this = this
+    this.setData({
+      song: {
+        duration: _this.data.songinfo.duration,
+        name: _this.data.songinfo.name
+      }
+    })
+    wx.setNavigationBarTitle({
+      title: _this.data.song.name
+    })
+    innerAudioContext.src = "http://aqqmusic.tc.qq.com/amobile.music.tc.qq.com/C400000lv3Zi13dSVA.m4a?guid=2012669392&vkey=CFF75E9E6AB9398FE6C5612A3F20D49D83C8CF43C568CA1DB8CF3FD78FCDF360AC47ECA384EC2331A49A32F09358C9C5CC62C67E1D9146E2&uin=0&fromtag=38"
+    // innerAudioContext.src = song.url
+    var currentTime = 'song.currentduration'
+    var currentprogres = 'song.progres'
     innerAudioContext.onPlay(() => {
-      console.log('onPlay')
+      console.log('play')
+    })
+    innerAudioContext.onTimeUpdate(() => {
+      var progres = (innerAudioContext.currentTime / innerAudioContext.duration) * 100
+      console.log(innerAudioContext.currentTime,innerAudioContext.duration )
+      _this.setData({
+        [currentTime]: innerAudioContext.currentTime,
+        [currentprogres]: progres
+      })
     })
   },
 
--- miniprogram/utils/common.js
+++ miniprogram/utils/common.js
@@ -1,18 +1,3 @@
-export function formatTimeSecond(second){
-	if(typeof second !== 'number') return false
-	var time = Math.ceil(second)
-	var remainder = time % 60
-	remainder = remainder >= 10 ? remainder : '0' + remainder
-	var result = Math.floor(time / 60)
-	var s = 0
-	if(result >= 1){
-		s =  Number(result + '.' + remainder)
-	} else {
-		s = Number(0 + '.' + remainder)
-	}
-	return s
-}
-
 export function singers(arr) {
   if(typeof arr !== 'object') return
   if(!arr.length) return
